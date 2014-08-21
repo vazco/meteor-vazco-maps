18246e6b0f2a6a517482446530c83883ee42e286
crx <me@webcrx.net>
2014-08-21 18:10:29 +0200

    added makrercluster lib

--- package.js
+++ package.js
@@ -4,5 +4,6 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.add_files(['vazco-maps.js'], 'client');
+    api.add_files(['markerclusterer.js'], 'client');
     api.export('VazcoMaps');
 });
